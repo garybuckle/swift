48ad02574d5d2b57375eb5af55dffa627030b7ef
garybuckle <buckle.g@mac.com>
2019-07-02 14:19:23 +0100

    Initial Commit

--- bmiCalc.playground/Contents.swift
+++ bmiCalc.playground/Contents.swift
@@ -1,9 +1,20 @@
 import UIKit
 
-func bmiCalc(height : Float, weight : Float) -> String {
-    var bmi = weight / pow(height, 2)
-    let shortBmi =  String(format: "%.2f", bmi)
-    
+func bmiCalc(height : Double, weight : Double) -> String {
+    let bmi = weight / pow(height, 2)
+   let shortBmi =  String(format: "%.2f", bmi)
     var evaluation = ""
-    return evaluation
+
+    if bmi > 25 {
+        evaluation = "overweight"
+    } else if bmi > 18.5 && bmi < 25 {
+        evaluation = "considered normal weight"
+    }
+    else {
+        evaluation = "underweight"
+    }
+    return "Your BMI is \(shortBmi)  and you are \(evaluation)"
 }
+
+
+var bmiResult = bmiCalc(height: 1.8, weight: 74)
