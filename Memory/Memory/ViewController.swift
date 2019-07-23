35f2612fd11d0272ea5202640bb0ffbd1cb4d284
garybuckle <buckle.g@mac.com>
2019-07-23 08:25:23 +0100

    Memory init

---
+++ Memory/Memory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memory
+//
+//  Created by Gary Buckle on 23/07/2019.
+//  Copyright © 2019 Gary Buckle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
